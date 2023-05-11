36c605a0119d1f61f26b67df01fdaf360499d71a
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-11 18:28:54 +0600

    banner section done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
   return (
     <div className="carousel h-[600px]">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} className="w-full" />
+        <img src={img1} className="w-full rounded-xl" />
         <div className="absolute flex items-center  h-full left-0  top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
           <div className="text-white space-y-7 pl-12 w-1/2">
             <h2 className="font-bold text-6xl">Affordable Price For Car Servicing</h2>
