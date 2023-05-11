f88d728cdae24f536e5e1b3161e4932b4dfd6704
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-05-11 12:57:02 +0600

    first commit done

---
+++ README.md
@@ -0,0 +1,1 @@
+"# car-doctot-clint-crud" 
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <h1>this is a home</h1>
+    </div>
+  );
+};
+
+export default Home;
